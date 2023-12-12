44cee01b76d8fe3b47c740a066312c05f9cfa8e2
pierrepinoli <pinoli.pierre@gmail.com>
2023-12-12 11:46:58 +0100

    avancement css du carrousel

--- src/components/Slideshow/Slideshow.jsx
+++ src/components/Slideshow/Slideshow.jsx
@@ -1,32 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState  } from 'react';
+import ArrowLeft from "../../assets/logo/chevron-left-solid.svg";
+import ArrowRight from "../../assets/logo/chevron-right-solid.svg";
 
 const Slideshow = ({ pictures }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   const nextSlide = () => {
+    
     setCurrentSlide((prevSlide) => (prevSlide + 1) % pictures.length);
   };
 
   const prevSlide = () => {
+    
     setCurrentSlide((prevSlide) => (prevSlide - 1 + pictures.length) % pictures.length);
   };
 
-  return (
-    <div className="slideshow">
-      <button onClick={prevSlide}>Précédent</button>
-      <div className="slide-container">
-        {pictures.map((url, index) => (
-          <div key={index} className={`slide ${index === currentSlide ? 'active' : ''}`}>
-            <img src={url} alt={`Slide ${index + 1}`} />
-          </div>
-        ))}
-      </div>
+  // const prevSlide = () => {
+    
+  //   setCurrentSlide(
+  //     currentSlide === 0 ? pictures.length - 1 : currentSlide - 1
+  //     );
+  // };
+
+  // const nextSlide = () => {
+
+  //   setCurrentSlide(
+  //     currentSlide === pictures.length - 1 ? 0 : currentSlide + 1
+  //     );
+  // };
+
   
 
-      <button onClick={nextSlide}>Suivant</button>
+  return (
+    <div className="slideshow">
+  
+      <img className="arrow arrow__left" src={ArrowLeft} alt="Flèche gauche" onClick={prevSlide}/>
+     
+
+      <div className="slide-container">
+      {pictures.map((url, index) => (
+         <img key={index} src={url} alt={`Slide ${index + 1}`} className={`slide ${index === currentSlide ? 'active' : ''}`} />
+                ))}
+      </div>
+      <img className="arrow arrow__right" src={ArrowRight} alt="Flèche droite" onClick={nextSlide}/>
+   
     </div>
   );
 };
 
 
 export default Slideshow;
+
--- src/pages/Housing/Housing.jsx
+++ src/pages/Housing/Housing.jsx
@@ -1,76 +1,3 @@
-// import React from 'react';
-// import Collapse from "../../components/Collapse/Collapse.jsx"
-// import Slideshow from "../../components/Slideshow/Slideshow.jsx";
-// import ExtractData from "../../components/ExtractData/ExtractData.jsx";
-// import data from '../../assets/logements.json';
-// import './housing.scss';
-
-// import { useParams } from 'react-router-dom';
-
-
-// const Housing = () => {
-//   // Utilisez le hook useParams pour extraire l'ID de l'URL
-//   const { id } = useParams();
-
-//   // Utilisez l'ID pour obtenir les données spécifiques de votre fichier JSON
-//   // Assurez-vous d'importer votre fichier JSON ou de passer les données en tant que prop
-//   // Utilisez les données pour afficher les détails spécifiques de l'hébergement
-
-//   return (
-
-//     <main>
-//               <div>
-//       <h2>Housing Details</h2>
-//       <p>ID: {id}</p>
-//       {/* Affichez les détails spécifiques en utilisant les données */}
-//     </div>
-//             <Slideshow pictures={data} />
-        
-//             <section className="section__housing">
-           
-//                 <div className="title__bar">
-//                     <div className="title__box">
-//                         <h2>Nom du logement</h2>
-//                     </div>
-                   
-//                     <div className="host__box">
-//                         <div className="host__name">
-//                             <p>Firt name</p>
-//                             <p>Last name</p>
-//                         </div>
-//                         <div className="host__pictureprofile"></div>
-//                     </div>
-//                 </div>  
-//                 <p>Lieu du logement</p>
-                
-//                 <div className="tag__line">
-//                     <div className="tag">Cozy</div>
-//                     <div className="tag">Canal</div>
-//                     <div className="tag">Paris 10</div>
-//                 </div>
-                
-//                 <div>Rating</div>
-//                 <div className="collapse__housing">
-//                     <Collapse title="Description">
-//                         <p>Texte de la Description</p>
-//                     </Collapse>
-//                     <Collapse title="Équipements">
-//                         <p>Texte des equipements.</p>
-//                     </Collapse>
-//                 </div>
-                
-//             </section>  
-
-
-//         </main>
-//     );
-// };
-
-// export default Housing;
-
-
-
-
 import React from 'react';
 import { Navigate } from 'react-router-dom'; // Importez le composant Navigate
 import Collapse from '../../components/Collapse/Collapse.jsx';
@@ -80,23 +7,22 @@ import './housing.scss';
 
 import { useParams } from 'react-router-dom';
 
+
+
 const Housing = () => {
   const { id } = useParams();
 
   // Utilisez l'ID pour obtenir les données spécifiques de votre fichier JSON
   const housingData = data.find(item => item.id === id);
-
+  console.log("Pictures:", housingData.pictures);
   // Si le logement n'est pas trouvé, redirigez vers la page 404
   if (!housingData) {
     return <Navigate to="/404" />;
   }
 
   return (
-    <main>
-      <div>
-        <h2>{housingData.title}</h2>
-        <p>ID: {id}</p>
-      </div>
+    <main className='main__housing'>
+
       <Slideshow pictures={housingData.pictures} />
 
       <section className="section__housing">
