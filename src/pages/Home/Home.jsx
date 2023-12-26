218dcad29fa81d5e086d8cd072afb518c6132324
pierrepinoli <pinoli.pierre@gmail.com>
2023-12-26 11:41:42 +0100

    deplacement de l'importation des données data du composant cards vers la page home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,28 +4,27 @@ import Cards from "../../components/Cards/Cards.jsx";
 
 // importations des fichiers annexes
 import bannerpicPath from "../../assets/images/cliffs.png";
+import data from '../../assets/logements.json';
 
 function Home () {
     
-     // établissement du DOM
+    // établissement du DOM
     return ( 
         <main className="main__home">
             <div className="banner__home">
-
                 {/* importation de la bannière */}
                 <Banner className="image--brightness" imageSrc={bannerpicPath} />
                 {/* inclusion d'une balise <br/> pour gérer le retour à la ligne via le css et le responsive */}
                 <h1>Chez vous, <br className="mobile-break"/> partout et ailleurs</h1>
-
             </div>
+            
             <section>
-
-                {/* importation du composant enfant Cards */}
-               <Cards />
+                {/* passage des données en tant que prop au composant enfant Cards */}
+                <Cards data={data} />
             </section>
         </main>
     )
 };
 
-//exportation du composant
+// exportation du composant
 export default Home;
