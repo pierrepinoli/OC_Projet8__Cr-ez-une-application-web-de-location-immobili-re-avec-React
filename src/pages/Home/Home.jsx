becbf15957f9764abe1a982e87decee60111a6c7
pierrepinoli <pinoli.pierre@gmail.com>
2023-12-27 09:26:12 +0100

    deplacement du map du composant card vers home + changement de fonction de card pour qu'elle ne crée qu'une seule carte

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,30 +1,27 @@
 // importations des dependances
 import Banner from "../../components/Banners/Banner.jsx";
-import Cards from "../../components/Cards/Cards.jsx";
+import Card from "../../components/Card/Card.jsx";
 
 // importations des fichiers annexes
 import bannerpicPath from "../../assets/images/cliffs.png";
 import data from '../../assets/logements.json';
 
-function Home () {
-    
-    // établissement du DOM
+function Home() {
     return ( 
         <main className="main__home">
             <div className="banner__home">
-                {/* importation de la bannière */}
                 <Banner className="image--brightness" imageSrc={bannerpicPath} />
-                {/* inclusion d'une balise <br/> pour gérer le retour à la ligne via le css et le responsive */}
                 <h1>Chez vous, <br className="mobile-break"/> partout et ailleurs</h1>
             </div>
             
             <section>
-                {/* passage des données en tant que prop au composant enfant Cards */}
-                <Cards data={data} />
+                <div className="cards__grid">
+                    {/* Utilisation de la fonction map pour créer les cartes de la grille en fonction du fichier logements.json*/}
+                    {data.map(item => <Card key={item.id} item={item} />)}
+                </div>
             </section>
         </main>
-    )
-};
+    );
+}
 
-// exportation du composant
 export default Home;
